4216f6ebb9f82994d110366b7fcce574db6ba7f0
mirkaNS <mirjana.g90@gmail.com>
2018-11-14 08:23:36 +0100

    ajax and jQuery

--- es6-moduls/ajax-jquery/main.js
+++ es6-moduls/ajax-jquery/main.js
@@ -1,16 +1,22 @@
 const $input = $("#text");
-const $button = $("#button");
+var $button = $("#button");
 const $gallery = $(".gallery");
 
-const $button = addEventListener("click", some);
+$button = addEventListener("click", some);
 
 
 function some() {
-
+    const $inputVal=$input.val();
     const request = $.ajax({
-        url: "https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000",
-        method: "GET",
-        dataType: "html"
+        url: "https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000"+ $inputVal
     });
+
+    request.done(function(data){
+        for(let i=0;i<data.items.length;i++){
+            $img=$("<img>");
+            $img.attr("src",data.items[i].avatar_url);
+            $gallery.append($img);
+        }
+    })
 }
 
